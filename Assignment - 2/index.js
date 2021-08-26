e002d76e729f08fda2e2b84b9a52e3d12cd9da92
Samarthjain0612 <84933489+Samarthjain0612@users.noreply.github.com>
2021-08-26 20:11:52 +0530

    Assignment-2

---
+++ Assignment - 2/index.js
@@ -0,0 +1,40 @@
+// create a array of objects with food data with properties
+
+let food = [
+    {
+        name: "Burger",
+        price: 60,
+        quantity: 1
+    },
+    {
+        name: "Sandwitch",
+        price: 40,
+        quantity: 1
+    },
+    {
+        name: "Butter-Paneer",
+        price: 130,
+        quantity: 1
+    },
+    {
+        name: "Cold-Coffee",
+        price: 70,
+        quantity: 1
+    },
+    {
+        name: "Veg Cheese Pizza",
+        price: 150,
+        quantity: 1
+    },
+
+    {
+        name: "Momos",
+        price: 60,
+        quantity: 8
+    }
+]
+
+for (let i = 0; i < food.length; i++)
+{
+    console.log(food[i]);
+}
